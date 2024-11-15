4e18a1e0cab7a315c39d384cf9d4ff83db8f4e5c
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-11-15 10:45:05 +0300

    Initial Commit

---
+++ SwiftChartsDateSnapping/SwiftChartsDateSnappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsDateSnappingApp.swift
+//  SwiftChartsDateSnapping
+//
+//  Created by Gene Bogdanovich on 15.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsDateSnappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
